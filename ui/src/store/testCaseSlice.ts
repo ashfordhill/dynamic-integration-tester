125f5a804d5c4426ac17750609768fa3e50c1c3f
ashfordhill <ashfordhill92@gmail.com>
2024-08-24 15:08:40 -0500

    finished manually flushing out the testCaseSlice file; wanna see if ChatGPT can be more efficient if I tell it to generate the rest of the reducers I need and a DataGrid with the relevant data w/o getting too liberal with its implementation choices

--- ui/src/store/testCaseSlice.ts
+++ ui/src/store/testCaseSlice.ts
@@ -1,38 +1,65 @@
-import { createSlice, PayloadAction } from '@reduxjs/toolkit';
-import { RootState } from './store';
-import { randomUUID } from 'crypto';
+import { createSelector, createSlice, PayloadAction } from '@reduxjs/toolkit'
+import { RootState } from './store'
+import { v4 as uuidv4 } from 'uuid'
+
+export type TestResultType = 'Pass' | 'Fail'
+
+interface TestResult {
+  id: string
+  testCaseId: string
+  result: TestResultType
+  resultMessage?: string
+}
 
 interface TestCase {
-  id: string;
-  inputFileName: string;
-  outputFileName: string | null;
+  id: string
+  inputFileName: string
+  outputFileName: string | null
 }
 
 interface TestCaseState {
-  testCases: TestCase[];
+  testCaseIds: string[]
+  testResultIds: string[]
+  testCases: Record<string, TestCase>
+  testResults: Record<string, TestResult>
 }
 
 const initialState: TestCaseState = {
-  testCases: [],
-};
+  testCaseIds: [],
+  testResultIds: [],
+  testCases: {},
+  testResults: {}
+}
 
-export const testCaseSliceName = "testCase"
+export const testCaseSliceName = 'testCase'
 const testCaseSlice = createSlice({
   name: testCaseSliceName,
   initialState,
   reducers: {
     addTestCase(state, action: PayloadAction<{ inputFileName: string; outputFileName: string | null }>) {
-      const id = `${Date.now()}`;
-      state.testCases.push({ id, ...action.payload });
-    },
-    removeTestCase(state, action: PayloadAction<string>) {
-      state.testCases = state.testCases.filter(testCase => testCase.id !== action.payload);
-    },
-    // Additional actions like editing, fetching, etc.
-  },
-});
+      const id = uuidv4()
+      state.testCaseIds.push(id)
+      state.testCases[id] = { ...action.payload, id }
+    }
+  }
+})
 
-export const { addTestCase, removeTestCase } = testCaseSlice.actions;
+export const { addTestCase } = testCaseSlice.actions
 
-export const selectTestCases = (state: RootState) => state[testCaseSliceName].testCases;
-export default testCaseSlice.reducer;
+export const selectTestCases = (state: RootState) => state[testCaseSliceName].testCases
+export const selectTestResults = (state: RootState) => state[testCaseSliceName].testResults
+export const selectTestCaseIds = (state: RootState) => state[testCaseSliceName].testCaseIds
+export const selectTestResultIds = (state: RootState) => state[testCaseSliceName].testResultIds
+
+export const selectTestResultsByTestCaseId = (testCaseId: string) =>
+  createSelector(selectTestResults, (testResults) =>
+    Object.values(testResults).filter((result) => result.testCaseId === testCaseId)
+  )
+
+export const selectTestResultById = (id: string) =>
+  createSelector(selectTestResults, (testResults) => Object.values(testResults).find((result) => result.id === id))
+
+export const selectTestCaseById = (id: string) =>
+  createSelector(selectTestResults, (testResults) => Object.values(testResults).find((result) => result.id === id))
+
+export default testCaseSlice.reducer
