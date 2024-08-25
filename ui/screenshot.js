0e03e3496319a27a69ffd0e377204f9a490f78dd
ashfordhill <ashfordhill92@gmail.com>
2024-08-25 14:56:48 -0500

    trying some settings to see if the headless env captures the image properly in Puppeteer

--- ui/screenshot.js
+++ ui/screenshot.js
@@ -1,9 +1,13 @@
 const puppeteer = require('puppeteer');
 
 (async () => {
-  const browser = await puppeteer.launch({ defaultViewport: { width: 1920, height: 1080 }});
+  const browser = await puppeteer.launch({ defaultViewport: { width: 1920, height: 1080 }, headless: true,
+    args: [
+        '--no-sandbox', // Required in some CI environments
+        '--disable-setuid-sandbox',
+      ]});
   const page = await browser.newPage();
-  await page.goto('http://localhost:3000');
+  await page.goto('http://localhost:3000', { waitUntil: 'networkidle2' });
   await page.screenshot({ path: 'screenshot.png' });
   await browser.close();
 })();
