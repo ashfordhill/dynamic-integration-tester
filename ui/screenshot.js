43edd06f7832c004d5a143146a048a3df149a5a8
ashfordhill <ashfordhill92@gmail.com>
2024-08-24 22:09:00 -0500

    try to get Puppeteer's screenshots to use a bigger window size

--- ui/screenshot.js
+++ ui/screenshot.js
@@ -1,7 +1,7 @@
 const puppeteer = require('puppeteer');
 
 (async () => {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({defaultViewport: null, headless: false});
   const page = await browser.newPage();
   await page.goto('http://localhost:3000');
   await page.screenshot({ path: 'screenshot.png' });
